1113660c05ad8af41b88ac5f732e72417efd9cf4
MostaSalama <super_mos2007@hotmail.com>
2022-08-18 12:35:29 +0200

    added sure content

---
+++ app/src/main/java/com/route/islamic36/home/quran/SuraDetails.kt
@@ -0,0 +1,55 @@
+package com.route.islamic36.home.quran
+
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+import android.widget.ScrollView
+import android.widget.TextView
+import com.route.islamic36.R
+import com.route.islamic36.adapter.SuraNameAdapter
+import java.io.IOException
+
+
+class SuraDetails : AppCompatActivity() {
+    lateinit var logo:ImageView
+    lateinit var suradetails:ScrollView
+    lateinit var sura:TextView
+     var suraNumber : Int = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sura_details)
+
+        logo = findViewById(R.id.logo)
+        suradetails = findViewById(R.id.suradetails)
+        sura = findViewById(R.id.Sura)
+        suraNumber = intent.getIntExtra("Sura_number",1)
+        Log.e("intent data passed",suraNumber.toString())
+        showSuraContent(suraNumber)
+
+    }
+
+
+    private fun showSuraContent(pos:Int) {
+
+        var suraContent : String = ""
+
+        try {
+            var suraNum : String = pos.toString()
+            val inputStream = assets.open("$suraNum.txt")
+            val size: Int = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.read(buffer).toString()
+            inputStream.close()
+            suraContent = String(buffer)
+
+        }
+        catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+        sura.text = suraContent
+    }
+}
